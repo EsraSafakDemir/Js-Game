5ccfb0221e7a1a784048fd01be675f2c54331ebf
EsraSafakDemir <67231993+EsraSafakDemir@users.noreply.github.com>
2020-12-11 21:33:12 +0300

    Add files via upload
    Demo Js pong

---
+++ ball.js
@@ -0,0 +1,18 @@
+function Ball(x,y, stick, stick1) {
+}
+
+Ball.prototype.randomDirection = function() {
+};
+
+Ball.prototype.move = function(){
+};
+
+Ball.prototype.pointScored = function(){
+};
+
+
+Ball.prototype.winner = function(){
+};
+
+Ball.prototype.restart = function(){
+};
---
+++ board.js
@@ -0,0 +1,17 @@
+function Board() {
+}
+
+Board.prototype.start = function(){
+};
+
+Board.prototype.draw = function(){
+};
+
+Board.prototype.setEvents = function(){
+};
+
+Board.prototype.rool = function(){
+};
+
+Board.prototype.loop = function(){
+};
